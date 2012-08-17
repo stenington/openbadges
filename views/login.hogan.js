0048cd65b3c18d5f35146344d04e7a4797b57caf
Mike Larsson <mikelarssonftw@gmail.com>
2012-08-17 14:55:05 -0400

    Bug fix: text sign in link on login page stopped working.

--- views/login.hogan.js
+++ views/login.hogan.js
@@ -1,5 +1,5 @@
 <h1>Welcome</h1>
-<h2>Use the green button below to <a href="#" class="js-browserid-link">sign in.</a><br/> Don&rsquo;t worry if you don&rsquo;t have an account, that&rsquo;ll get taken care of.</h2>
+<h2>Use the green button below to <a href="#" class="js-persona-login">sign in.</a><br/> Don&rsquo;t worry if you don&rsquo;t have an account, that&rsquo;ll get taken care of.</h2>
 
 <div style="padding-top: 10px">
   <a class="js-persona-login" data-csrf="{{ csrfToken }}" href="#"><img src="https://browserid.org/i/sign_in_green.png"/></a>
