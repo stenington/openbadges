1610742315df4433755633a97810cb0111dd3e90
Mike Larsson <mikelarssonftw@gmail.com>
2012-10-29 17:51:28 -0400

    Test probe for travis

---
+++ test.sql
@@ -0,0 +1,4 @@
+create database timetest;
+use timetest
+create table times (id integer primary key auto_increment, mytime datetime);
+insert into times (mytime) values ('2012-10-25T16:25:16.357Z');
