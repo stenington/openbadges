2622267a6c7cdb69500205d3b8a3838fe628ed82
Mike Larsson <mikelarssonftw@gmail.com>
2012-10-29 18:03:56 -0400

    Trying to turn off traditional mode

--- test.sql
+++ test.sql
@@ -1,4 +1,5 @@
-create database timetest;
-use timetest
-create table times (id integer primary key auto_increment, mytime datetime);
+create database if not exists timetest;
+use timetest;
+set global sql_mode='';
+create table if not exists times (id integer primary key auto_increment, mytime datetime);
 insert into times (mytime) values ('2012-10-25T16:25:16.357Z');
